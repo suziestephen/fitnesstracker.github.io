ee7eccbfe2725200c3f38b33a76181c216368b77
suziestephen <suzietstephen@gmail.com>
2021-04-05 21:20:52 +1000

    starting workout.js file

---
+++ models/workout.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
