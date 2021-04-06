15fd42abd7afc33f4028ceef832ed4de75836e2c
suziestephen <suzietstephen@gmail.com>
2021-04-06 16:10:55 +1000

    removing excercise schema back to own file

--- models/workout.js
+++ models/workout.js
@@ -8,35 +8,7 @@ const workoutSchema = new Schema({
             type: Date,
             default: Date.now
         },
-    
-        exercises: [{
-            type: {
-                type: String,
-                trim: true,
-                required: "Enter completed exercise"
-            },
-            name: {
-                type: String,
-                trim: true,
-                required: "Enter exercise name"
-            },
-            duration: {
-                type: Number,
-                required: "Enter workout length"
-            },
-            weight: {
-                type: Number,
-            },
-            reps: {
-                type: Number,
-            },
-            sets: {
-                type: Number,
-            },
-            distance: {
-                type: Number,
-            }
-        }]
+
     });
     
 
