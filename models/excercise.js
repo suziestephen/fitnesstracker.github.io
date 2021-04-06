c5f38cc07a743cc4f21ab6f724280741c565464d
suziestephen <suzietstephen@gmail.com>
2021-04-06 16:13:55 +1000

    fixing exercise schema

--- models/excercise.js
+++ models/excercise.js
@@ -3,13 +3,36 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const excerciseSchema = new Schema({
-    type: String,
-    name: String,
-    duration: String,
-    distance: String,
-    weight: String,
-    reps: String,
-    set: String,
+    type:  {
+            type: String,
+            trim: true,
+            required: "Enter your completed exercise"
+    },
+
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter your excercise type"
+    },
+    duration: {
+        type: Number,
+        required: "Enter workout length"
+    },
+
+
+    distance: {
+        type: Number,
+    },
+    
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    set: {
+        type: Number,
+    },
 });
 
 const Excercise = mongoose.model("Exercise", excerciseSchema);
