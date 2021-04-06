f6feffa34f598ac085ae511682190ea4cfe8796b
suziestephen <suzietstephen@gmail.com>
2021-04-06 15:13:39 +1000

    models/workout schema

--- models/workout.js
+++ models/workout.js
@@ -3,8 +3,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  
-});
+
+        day: {
+            type: Date,
+            default: Date.now
+        },
+    
+        exercises: [{
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter completed exercise"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter exercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Enter workout length"
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }]
+    });
+    
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
